4b86b649a6b2ccd10ef66186e7adb2e7d58d36c1
Pedro Henrique <pedroabreu.dev@gmail.com>
2021-09-19 01:10:45 -0400

    Componente de lista - init

---
+++ src/screens/Basket/componentes/Itens.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import { View, Text } from 'react-native'
+import Texto from '../../../componentes/Texto'
+
+export default function itens() {
+    return <>
+        <Texto>Itens da cesta</Texto>
+    
+    </>
+}
--- src/screens/Basket/index.js
+++ src/screens/Basket/index.js
@@ -1,13 +1,15 @@
 import React from 'react';
 import { StyleSheet, View } from 'react-native';
 import Detalhes from './componentes/Detalhes';
+import Itens from './componentes/Itens';
 import Topo from './componentes/Topo';
 
 export default function Basket({topo, detalhes}) {
     return <>
         <Topo {...topo}/>  
         <View style={styles.cesta}>
-            <Detalhes {...detalhes}/>            
+            <Detalhes {...detalhes} />
+            <Itens />            
         </View>
 
     </>
